56ae97d241d2a178030de8333f896ce940fa8254
Agata Przybyszewska <agata@mathmagicians.dk>
2017-09-14 12:53:05 +0200

    Added springboot support

---
+++ src/main/java/dk/mathmagicians/kumori/KumoriApplication.java
@@ -0,0 +1,12 @@
+package dk.mathmagicians.kumori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KumoriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KumoriApplication.class, args);
+	}
+}
