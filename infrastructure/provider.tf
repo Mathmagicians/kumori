0e326948c71a573ed96592b21550df7c1077d713
Johannes Frandsen <jsf@greenoak.dk>
2018-05-05 06:37:56 +0200

    Attach persisten volume when creating infrastructure.

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -5,6 +5,7 @@ variable "ssh_fingerprint" {}
 variable "cloudflare_email" {}
 variable "cloudflare_token" {}
 variable "cloudflare_domain" {}
+variable "kumori_volume" {}
 
 provider "digitalocean" {
   token = "${var.do_token}"
