656f240aac0b9f4f33193e7ff9eeefa0912af93b
Agata Przybyszewska <agata@mathmagicians.dk>
2018-09-12 01:45:55 +0200

    Bootstrap working, but css styles do not get loaded ...

--- meritocracy/frontend/src/App.vue
+++ meritocracy/frontend/src/App.vue
@@ -1,17 +1,17 @@
 <template>
 	<div id="app">
-		<header>
-			<router-link to="/">Meritocracy</router-link>
-		</header>
-		<b-nav>
-			<b-nav-item>
-				<router-link to="/">Meritocracy</router-link>
-			</b-nav-item>
-			<b-nav-item><router-link to="/components">Components</router-link></b-nav-item>
-			<b-nav-item><router-link to="/services">Services</router-link></b-nav-item>
-			<b-nav-item><router-link to="/exterminator">Exterminator</router-link></b-nav-item>
-			<b-nav-item><router-link to="/about">About</router-link></b-nav-item>
-		</b-nav>
+		<b-nav-bar type="light" variant="faded" fixed="top">
+			<b-navbar-brand to="/">
+				<img src="./assets/techmenu.svg">
+			</b-navbar-brand>
+			<b-navbar-nav>
+				<b-nav-item><router-link to="/">Meritocracy</router-link></b-nav-item>
+				<b-nav-item><router-link to="/components">Components</router-link></b-nav-item>
+				<b-nav-item><router-link to="/services">Services</router-link></b-nav-item>
+				<b-nav-item><router-link to="/exterminator">Exterminator</router-link></b-nav-item>
+				<b-nav-item><router-link to="/about">About</router-link></b-nav-item>
+			</b-navbar-nav>
+		</b-nav-bar>
 		<router-view/>
 	</div>
 </template>
