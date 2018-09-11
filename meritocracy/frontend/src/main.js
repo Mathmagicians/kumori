88f4fd64a61397b2fe59519db8b329f5fc413a2c
Agata Przybyszewska <agata@mathmagicians.dk>
2018-09-11 10:30:10 +0200

    Added bootstrap-vue support

--- meritocracy/frontend/src/main.js
+++ meritocracy/frontend/src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import App from './App'
 import router from './router'
 
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
+/* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
