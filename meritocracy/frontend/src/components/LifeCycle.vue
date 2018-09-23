a46b2da35a05c9770e255abe480e010d81b9bb35
Johannes Frandsen <jsf@greenoak.dk>
2018-09-23 12:28:21 +0200

    Small tweaks to ui

--- meritocracy/frontend/src/components/LifeCycle.vue
+++ meritocracy/frontend/src/components/LifeCycle.vue
@@ -1,9 +1,9 @@
 <template>
-  <span>
+<span>
     <span v-if="lc">
       <b-button
         size="sm"
-        class="md-4 px-2 align-middle btn-lifecycle"
+        class="md-4 px-2 align-middle lifecycle"
         v-b-popover.hover="popup"
         :variant="btn"
         :to="to"
@@ -11,79 +11,93 @@
         {{status | capitalize}}
       </b-button>
     </span>
-     <span v-else >
+    <span v-else >
         <b-button
           size="sm"
-          class="md-4 px-2 align-middle btn-lifecycle"
+          class="md-4 px-2 align-middle lifecycle"
           variant="outline-danger">
           Missing
       </b-button>
     </span>
-  </span>
+</span>
 </template>
 
 <script>
+import lifeCycleMixin from '../mixins/lifeCycle.js'
 
-  import lifeCycleMixin from '../mixins/lifeCycle.js'
-
-	export default {
-		name: 'lifeCycle',
-		props: {
-      status: {required: true},
-      to: {type:String, required:false},
-      ispopup: {type: Boolean, required: false, default: false}
+export default {
+  name: 'lifeCycle',
+  props: {
+    status: {
+      required: true
     },
-    mixins: [
-      lifeCycleMixin
-    ],
-    lcText: {
-      'experimental': `A brand new, (possibly) bleeding edge technology that we want to investigate.`,
-      'testing': `
+    to: {
+      type: String,
+      required: false
+    },
+    ispopup: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  },
+  mixins: [
+    lifeCycleMixin
+  ],
+  lcText: {
+    'experimental': `A brand new, (possibly) bleeding edge technology that we want to investigate.`,
+    'testing': `
       <p>This is the part of the lifecycle, where we try out things.</p>
       <p>The technology seems to have <strong> significant advantages</strong> for some use cases, that are superior over the currently approved technologies. We want to explore this to learn more.</p>
       `,
-      'poc': `
+    'poc': `
       <p>A technology, with well known characteristics, but either the technology or the operational capabilities / knowledge are not yet mature enough for full-scale production or critical applications.
       </p>
       <p>In order to gain a better understanding, we should do a Proof Of Concept - launch the technology in production, for a suitably chosen application.
       </p>
       `,
-      'default': `A mature technology, with well known characteristics, for which Nordea has a mature operational capability.
+    'default': `A mature technology, with well known characteristics, for which Nordea has a mature operational capability.
       No further questions asked, you can go ahead and use this technology to build your applications, for all the defined use cases.
       `,
-      'limited': `A mature technology, with well known characteristics, for which there is amature operational capability, but which for various reasons <em>(cost, licensing, strategic direction, etc, </em> it is desirable to limit the exceptional situations and use cases in which it is used.`,
-      'deprecated': `A technology that we are actively phasing out (e.g. due to lack of support, security, stability, cost issues, impossible to staff, very inefficient in use or to be replaced with another technology, etc.)
+    'limited': `A mature technology, with well known characteristics, for which there is amature operational capability, but which for various reasons <em>(cost, licensing, strategic direction, etc, </em> it is desirable to limit the exceptional situations and use cases in which it is used.`,
+    'deprecated': `A technology that we are actively phasing out (e.g. due to lack of support, security, stability, cost issues, impossible to staff, very inefficient in use or to be replaced with another technology, etc.)
       `,
-      'dontuse': `Must not be used, either for maintenance, or for new development. This is typically due to legal, policy or architecture reasons.`,
-      'wip': `#techmenu is currently working on updating the life cycle state of this technology`,
-      'undecided': `#techmenu has no opinion about this technology. Do we need it? Let us get this into #techmenu.`
+    'dontuse': `Must not be used, either for maintenance, or for new development. This is typically due to legal, policy or architecture reasons.`,
+    'wip': `#techmenu is currently working on updating the life cycle state of this technology`,
+    'undecided': `#techmenu has no opinion about this technology. Do we need it? Let us get this into #techmenu.`
+  },
+  computed: {
+    lc: function() {
+      return this.$store.state.lifeCycle.items.filter(item => item.name === this.status).pop();
     },
-    computed: {
-       lc: function(){
-        return this.$store.state.lifeCycle.items.filter( item => item.name === this.status).pop();
-      },
 
-      btn: function() {
-       return this.btnVariant(this.lc? this.lc.type: 'maybe');
-      },
-
-      popup: function(){
-        return this.ispopup?this.$options.lcText[this.lc.name]:'';
-      }
-
+    btn: function() {
+      return this.btnVariant(this.lc ? this.lc.type : 'maybe');
     },
-    filters: {
-      capitalize: function( lower){
-        return lower.charAt(0).toUpperCase() + lower.substr(1);
-      },
-      toUpperCase: function(upper){
-        return upper.toUpperCase();
-      },
-      title: function( title){
-        return 'Life cycle phase '+title;
-      }
 
+    popup: function() {
+      return this.ispopup ? this.$options.lcText[this.lc.name] : '';
     }
-	}
 
+  },
+  filters: {
+    capitalize: function(lower) {
+      return lower.charAt(0).toUpperCase() + lower.substr(1);
+    },
+    toUpperCase: function(upper) {
+      return upper.toUpperCase();
+    },
+    title: function(title) {
+      return 'Life cycle phase ' + title;
+    }
+
+  }
+}
 </script>
+
+<style scoped>
+.lifecycle {
+	min-width: 8em;
+  max-width: 8em;
+}
+</style>
--- meritocracy/frontend/src/components/TechComponent.vue
+++ meritocracy/frontend/src/components/TechComponent.vue
@@ -1,16 +1,17 @@
 <template>
-	<div :id="id">
-		<b-card no-body>
-			<p slot="header" v-b-toggle="accordionId">
-				<b-button v-on:click="changeRoute" variant="link lg">{{tech.name}}</b-button>
+<div :id="id">
+  <b-card no-body>
+    <span slot="header" v-b-toggle="accordionId">
+			<b-button size="sm" v-on:click="changeRoute" variant="link">{{tech.name}}</b-button>
 				<life-cycle :status="tech.status" to="about"></life-cycle>
-			</p>
-			<b-collapse :id="accordionId" accordion="tech" v-on:show="changeRoute" v-on:click="changeRoute" :visible="active?active:false">
-				<div>
-					<p class="card-text">
-						{{tech}} 
-					</p>
-					<div slot="footer" class="card-footer">
+		</span>
+
+    <b-collapse :id="accordionId" accordion="tech" v-on:show="changeRoute" v-on:click="changeRoute" :visible="active?active:false">
+      <div>
+        <p class="card-text">
+          {{tech}}
+        </p>
+        <span slot="footer" class="card-footer">
 						<b-button variant="outline-secondary sm" class="btn-round">
 		            		<v-icon name="heart" scale="1"/></v-icon>
 		        		</b-button>
@@ -21,35 +22,49 @@
 		            		<v-icon name="comment" scale="1"/></v-icon>
 		        		</b-button>
 		        	</div>
-	        	</div>
-        	</b-collapse>
-        </b-card>
-	</div>
+	        	</span>
+    </b-collapse>
+  </b-card>
+</div>
 </template>
 
 <script>
+import LifeCycle from '../components/LifeCycle.vue'
 
-	import LifeCycle from '../components/LifeCycle.vue'
-
-	export default {
-  		name: 'techComponent',
-  		components: {
-  			LifeCycle
-  		},
-  		props: {
-  			id: { type: String, required: true},
-  			tech: { type: Object, required: true}, 
-  			active: {type: Boolean, default: false}
-  		},
-  		computed: {
-  			accordionId: function(){
-  				return 'accordion-' + this.tech.uid; 
-  			}
-  		}, 
-  		methods: {
-  			changeRoute: function() {
-  				this.$router.push('/components/'+ this.tech.uid);
-  			}
-  		}
-  	}
+export default {
+  name: 'techComponent',
+  components: {
+    LifeCycle
+  },
+  props: {
+    id: {
+      type: String,
+      required: true
+    },
+    tech: {
+      type: Object,
+      required: true
+    },
+    active: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    accordionId: function() {
+      return 'accordion-' + this.tech.uid;
+    }
+  },
+  methods: {
+    changeRoute: function() {
+      this.$router.push('/components/' + this.tech.uid);
+    }
+  }
+}
 </script>
+
+<style scoped>
+.test {
+  background-color: red;
+}
+</style>
