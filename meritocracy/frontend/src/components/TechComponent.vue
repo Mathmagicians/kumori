d6918dabbeeb0bad50832c6012d8cca1d128a86d
Agata Przybyszewska <agata@mathmagicians.dk>
2018-09-23 12:44:59 +0200

    fixed span error

--- meritocracy/frontend/src/components/TechComponent.vue
+++ meritocracy/frontend/src/components/TechComponent.vue
@@ -4,7 +4,7 @@
     <span slot="header" v-b-toggle="accordionId">
 			<b-button size="sm" v-on:click="changeRoute" variant="link">{{tech.name}}</b-button>
 				<life-cycle :status="tech.status" to="about"></life-cycle>
-		m</span>
+		</span>
 
     <b-collapse :id="accordionId" accordion="tech" v-on:show="changeRoute" v-on:click="changeRoute" :visible="active?active:false">
       <div>
@@ -21,8 +21,9 @@
 		        		<b-button  variant="outline-secondary sm" class="btn-round">
 		            		<v-icon name="comment" scale="1"/></v-icon>
 		        		</b-button>
+                </span>
 		        	</div>
-	        	</span>
+	        	
     </b-collapse>
   </b-card>
 </div>
