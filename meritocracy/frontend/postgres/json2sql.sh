bb07461d3e686beef1ca0b508e3b7d8e1a4c1362
Johannes Skov Frandsen <localgod@heaven.dk>
2018-10-03 11:13:56 +0200

    Update json2sql.sh

--- meritocracy/frontend/postgres/json2sql.sh
+++ meritocracy/frontend/postgres/json2sql.sh
@@ -1,10 +1,59 @@
 #!/usr/bin/env bash
-apt-get update
-apt-get install jq
-if [ -f postgres/fixture.sql ]; then rm postgres/fixture.sql; fi
-echo '-- Auto generated' >> postgres/fixture.sql
-echo '\connect meritocracy;' >> postgres/fixture.sql
-while read -r component; do
-  data="$(echo ${component} | base64 --decode | jq -r -c '.')"
-  echo -e "INSERT INTO api.components (data) VALUES ('${data//\'/\'\'}');" >> postgres/fixture.sql
-done <<< "$(jq -r '.[] | @base64' < src/api/mock/data/techComponents.json)"
+PGDATABASE='meritocracy'
+PGPASSWORD='postgres'
+PG_HOST='postgres'
+
+function _commit () {
+  psql -U ${PGPASSWORD} -h ${PG_HOST} -d ${PGDATABASE} -t -q -c "${1}"
+}
+
+function components () {
+  _commit "DELETE FROM api.comments;"
+  _commit "DELETE FROM api.components;"
+
+  while read -r component; do
+    local name="$(echo ${component} | base64 --decode | jq -r -c '.name')"
+    local description="$(echo ${component} | base64 --decode | jq -r -c '.description')"
+    if [ "${description}" == '' ]
+    then
+      description='Missing'
+    fi
+
+    _commit "INSERT INTO api.components (name, description, deleted) VALUES (\$tag\$${name}\$tag\$,\$tag\$${description}\$tag\$,false);"
+
+    local component_id="$(_commit "SELECT id from api.components order by id desc limit 1;" | awk '{$1=$1};1')"
+
+    local comments="$(echo ${component} | base64 --decode | jq -r -c '.comments')"
+
+    if [ "${comments}" != '' ]
+    then
+      comment "${comments}" "${component_id}"
+    fi
+
+  done <<< "$(cat src/api/mock/data/techComponents.json | jq -r '.components[] | @base64')"
+}
+
+# Insert a comment
+function comment () {
+  _commit "INSERT INTO api.comments (comment, component,deleted) VALUES (\$tag\$${1}\$tag\$,\$tag\$${2}\$tag\$,false);"
+}
+
+# Insert a usecase
+function usecase () {
+  _commit "INSERT INTO api.usecases (comment, component) VALUES (\$tag\$${1}\$tag\$,\$tag\$${2}\$tag\$);"
+}
+
+# Create taxonomy
+function taxonomi () {
+  _commit "DELETE FROM api.taxonomy;"
+  while read -r component; do
+    name="$(echo ${component} | base64 --decode | jq -r -c '.name')"
+    level="$(echo ${component} | base64 --decode | jq -r -c '.level')"
+    parent="$(echo ${component} | base64 --decode | jq -r -c '.parent')"
+    _commit "INSERT INTO api.taxonomy (name, level, parent) VALUES (\$tag\$${name}\$tag\$,\$tag\$${level}\$tag\$,\$tag\$${parent}\$tag\$);"
+  done <<< "$(src/api/mock/data/taxonomy.json | jq -r '.tags[] | @base64')"
+}
+
+
+components
+taxonomi
