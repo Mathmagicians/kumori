d4442f32493599d0f2a94199c31fe337d764b883
Johannes Skov Frandsen <localgod@heaven.dk>
2018-10-03 11:03:36 +0200

    Create row_inserted.sql

---
+++ meritocracy/frontend/postgres/functions/row_inserted.sql
@@ -0,0 +1,12 @@
+SET client_min_messages TO WARNING;
+DROP FUNCTION IF EXISTS row_inserted () CASCADE;
+
+CREATE FUNCTION row_inserted ()
+    RETURNS TRIGGER
+    LANGUAGE plpgsql
+AS $$
+BEGIN
+    NEW.modified := LOCALTIMESTAMP;
+    RETURN NEW;
+END;
+$$;
