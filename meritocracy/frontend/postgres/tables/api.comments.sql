ea2eb04ee0a9816e89be7c12eaeea23b99c74347
Johannes Skov Frandsen <localgod@heaven.dk>
2018-10-03 11:04:44 +0200

    Create api.comments.sql

---
+++ meritocracy/frontend/postgres/tables/api.comments.sql
@@ -0,0 +1,25 @@
+SET client_min_messages TO WARNING;
+DROP TABLE IF EXISTS api.comments CASCADE;
+CREATE TABLE api.comments
+(
+    id serial NOT NULL,
+    comment text NOT NULL,
+    component integer NOT NULL,
+    modified timestamp without time zone NOT NULL,
+    deleted boolean NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT component FOREIGN KEY (component)
+        REFERENCES api.components (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+);
+
+DROP TRIGGER IF EXISTS comments_insert ON api.comments;
+CREATE TRIGGER comments_insert BEFORE INSERT
+  ON api.comments FOR EACH ROW EXECUTE PROCEDURE row_inserted ();
+
+DROP TRIGGER IF EXISTS comments_update ON api.comments;
+CREATE TRIGGER comments_update BEFORE UPDATE
+  ON api.comments FOR EACH ROW EXECUTE PROCEDURE row_updated ();
+
+GRANT SELECT (id, comment, component, modified, deleted) ON api.comments TO web_anon;
