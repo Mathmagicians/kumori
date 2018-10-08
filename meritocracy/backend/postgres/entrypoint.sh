8c46ee64b8e47165ce3b4240761c37495716b41b
Johannes Skov Frandsen <localgod@heaven.dk>
2018-10-08 13:05:19 +0200

    Update entrypoint.sh

--- meritocracy/backend/postgres/entrypoint.sh
+++ meritocracy/backend/postgres/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-RUN sed  -i "s/usename\s=\s'meritocracy'/usename = '${DB_CRUD_USER}'/" /docker-entrypoint-initdb.d/01.sql
+find . -type f -name "*.sql" -print0 | xargs -0 sed -i "s/meritocracy/${DB_CRUD_USER}/g"
+find . -type f -name "*.sql" -print0 | xargs -0 sed -i "s/meritocracy/${DB_R_USER}/g"
+
+sed  -i "s/PASSWORD\s'${DB_CRUD_USER}'/PASSWORD '${DB_CRUD_PASS}/" /docker-entrypoint-initdb.d/01.sql
 
 ./docker-entrypoint.sh
