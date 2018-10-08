1c5aecd97d2528d90eb92c219b10ce05b7051df7
Johannes Skov Frandsen <localgod@heaven.dk>
2018-10-08 16:09:18 +0200

    Will return empty array instead of null values

--- meritocracy/backend/postgres/views/api.w_components.sql
+++ meritocracy/backend/postgres/views/api.w_components.sql
@@ -9,25 +9,31 @@ SELECT
     component.name as name,
     status.name as status,
     component.description,
-    (
-        SELECT array_to_json(array_agg(co.comment))
-        FROM api.comments AS co
-        WHERE co.component = component.id
-    ) as comments,
-    (
-        SELECT array_to_json(array_agg(tx.name))
+	(
+	 CASE WHEN 0 < (SELECT COUNT(co.id) FROM api.comments AS co WHERE co.component = component.id)
+	 THEN (SELECT array_to_json(array_agg(co.comment))
+            FROM api.comments AS co
+            WHERE co.component = component.id)
+	 ELSE (json_build_array()) END
+	) AS comments,
+	(
+	 CASE WHEN 0 < (SELECT COUNT(ct.component) FROM api.component_taxonomy AS ct WHERE ct.component = component.id)
+	 THEN (SELECT array_to_json(array_agg(tx.name))
         FROM api.component_taxonomy AS ct
         JOIN api.taxonomy AS tx ON ct.taxonomy = tx.id
-        WHERE ct.component = component.id
-    ) as tags,
+        WHERE ct.component = component.id)
+	 ELSE (json_build_array()) END
+	) AS tags,
     json_build_array() AS licenses,
-    (
-      SELECT array_to_json(array_agg(json_build_object(
+	(
+	 CASE WHEN 0 < (SELECT COUNT(co.id) FROM api.links AS co WHERE co.component = component.id)
+	 THEN (SELECT array_to_json(array_agg(json_build_object(
         'type',co.type,
         'ref',co.ref)))
       FROM api.links AS co
-      WHERE co.component = component.id
-    ) AS links,
+      WHERE co.component = component.id)
+	 ELSE (json_build_array()) END
+	) AS links,
     (
       SELECT array_to_json(array_agg(json_build_object(
         'name',us.name,
