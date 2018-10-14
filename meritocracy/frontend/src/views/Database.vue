5ade0291f938169b1c41a3b8a58571800bec4ab4
Agata Przybyszewska <agata@mathmagicians.dk>
2018-10-14 21:13:24 +0200

    Pagination functionality works with api

--- meritocracy/frontend/src/views/Database.vue
+++ meritocracy/frontend/src/views/Database.vue
@@ -1,33 +1,35 @@
 <template>
 <div>
   <h1>Demo access to database</h1>
-  <div v-if="tech.length === 0">No data</div>
+  <div v-if="page.loading">Loading data ... </div>
   <div v-else>
     <h4>Found {{tech.length}} items:</h4>
-      <h6>I alt {{page.total}}, page size: {{page.size}}, viser fra </h6>
-   <b-button-toolbar>
-      <b-button-group class="mx-1">
-        <b-btn 
-          v-for="(el,index) in pages" 
-          :key="index" 
-          @click="scrollToPage(index)"
-          variant="link">
-          {{pages[index].from}} - {{pages[index].to}} 
-        </b-btn>
-      </b-button-group>
+      <h6>I alt {{page.total}}, page size: {{page.size}}, viser page {{page.activePageIndex}} </h6>
+   
+      <b-button-toolbar>
+        <b-button-group class="mx-1">
+          <b-btn 
+            v-for="i in numOfPages" 
+            :key="i" 
+            @click="scrollToPage(i-1)"
+            variant="link">
+             {{i-1}}: {{ buttonName(i-1)}} 
+          </b-btn>
+        </b-button-group>
     </b-button-toolbar>
-    <b-list-group>
-      <b-list-group-item 
-        v-for="index in indexesForPage(page.activePageIndex)"
-        :active="index===page.activeIndex"
-        >
-        {{index+1}}: {{tech[index].name}} | {{tech[index].status}}
-        <b-badge class="mx-1"
-          v-for="t in tech[index].tags">
-          {{t}}
-        </b-badge>
-        <v-icon v-if="page.activeIndex=== index" name="clipboard" scale="2" color="orange"/>
-      </b-list-group-item>
+
+      <b-list-group>
+        <b-list-group-item 
+          v-for="(t, index) in tech" 
+          :active="index===page.activeIndex"
+          >
+          {{pi2page(page.activePageIndex)+index+1}}: {{t.name}} | {{t.status}}
+          <b-badge class="mx-1"
+            v-for="t in t.tags">
+            {{t}}
+          </b-badge>
+          <v-icon v-if="page.activeIndex=== index" name="clipboard" scale="2" color="orange"/>
+        </b-list-group-item>
   </b-list-group>
   </div>
 </div>
@@ -40,50 +42,56 @@
       return {
         tech: [],
         page: {
+          loading: false,
           size: 10,
-          from: 0,
-          total: 101,
-          activeIndex: 0,
-          activeUid: null,
-          activePage: {from:0, to:0},
+          total: 0,
           activePageIndex: 0
         }
       }
     },
+
     computed: {
-      pages() {
-        return this.pagesArray( this.page.total, this.page.size)
+      numOfPages(){
+        return Math.ceil(this.page.total/this.page.size)
       }
     },
+
     created () {
-      this.getHeaders(this.pages[0].from, this.pages[0].to)
+      this.scrollToPage(this.page.activePageIndex)
     },
+
+
     methods: {
-      /*fixme: unit test: input (42,10) out  [ { "from": 1, "to": 10 }, { "from": 11, "to": 20 }, { "from": 21, "to": 30 }, { "from": 31, "to": 40 }, { "from": 41, "to": 42 } ]
+       buttonName( i ){
+        let page = this.shows( i, this.page.size, this.page.total)
+        return `${page.from} - ${page.to}`
+      },
+       /*fixme: unit test: input (42,10) out  [ { "from": 1, "to": 10 }, { "from": 11, "to": 20 }, { "from": 21, "to": 30 }, { "from": 31, "to": 40 }, { "from": 41, "to": 42 } ]
         for( 101,10)
          [ { "from": 1, "to": 10 }, { "from": 11, "to": 20 }, { "from": 21, "to": 30 }, { "from": 31, "to": 40 }, { "from": 41, "to": 50 }, { "from": 51, "to": 60 }, { "from": 61, "to": 70 }, { "from": 71, "to": 80 }, { "from": 81, "to": 90 }, { "from": 91, "to": 100 }, { "from": 101, "to": 101 } ]
           */
-      pagesArray (total, size) {
-        const totalPages = Math.ceil(total/size)
-        return [...Array(totalPages).keys()].map( p => ({...this.shows(p,size, total)}))
+      pi2page(pageIndex) {
+        return pageIndex*this.page.size
       },
-      indexesForPage( pi ){
-        const start = this.pages[pi].from -1
-        const end = this.pages[pi].to 
-        return Array(end - start).fill().map((d, i) => i + start)
-      },
-      shows(pageIndex, pageSize, max){
-        const pageStart = pageIndex*pageSize
-        const pageEnd = pageStart+ pageSize < max ? pageStart+pageSize : max
-        return { from: pageStart + 1, to: pageEnd }
+      shows(index, size, max){
+        console.log(`>>>shows>>>: index ${index}, size ${size}, max ${max}`)
+        const maxSize = Math.max(size, max)
+        const from = this.pi2page(index) +1
+        const to = Math.min( this.pi2page(index+1) , maxSize)
+        const indexes = {from, to}
+        console.log(`>>> shows >>> from : ${indexes.from}, to: ${indexes.to}`)
+        return indexes
       },
       scrollToPage(index){
-        this.page.activePage = this.pages[index]
+        this.page.loading = true
         this.page.activePageIndex = index
-        this.getHeaders(this.page.activePage.from-1, this.page.activePage.to-1)
+        console.log(`>>> scroll to page>>> ${index}`)
+        let newPageIndexes = this.shows(index, this.page.size, this.page.total)
+        this.getHeaders(newPageIndexes.from-1, newPageIndexes.to-1)
+        //this.tech.forEach((t,i) => this.tech[i].name = `00${i} --- ${t.name}`)
       },
 
-      getHeaders: function(from=1, to=19) {
+      getHeaders: function(from=0, to=10) {
         // Prefer: count=exact
         // Range-Unit: items
         //Range: 0-19
@@ -93,27 +101,26 @@
           'Range': `${from}-${to}`
         }}
 
-        this.$http.get('http://0.0.0.0:3000/w_components?select=name,status,tags', config).then(response => {
-          this.tech = response.body;
-          console.log(response.headers['content-range'])
-          console.log(response.headers)
-          this.tech.forEach((t,i) => this.tech[i].name = `00${i} --- ${t.name}`)
-         
-           console.log(response.headers)
-          console.log("printing headers")
-          response.headers.forEach( (k,v) => console.log(`${k}:\t ${v}`))
+        let headersResponse = {headers: [], total: 0}
+
+        console.log(`>>> getHeaders >>> from ${from}, to ${to}`)
+
+        this.$http.get('http://0.0.0.0:3000/w_components?select=name,status,tags', config)
+        .then(response => {
           console.log(`We are interested in ${response.headers.get('content-range')} `)
           let range = response.headers.get('content-range')
           let rs = range.split('/')
           let total = rs[1]
-          this.page.total = total
+          this.page.total = Number(total)
           let current = rs[0]
           let page = { from: rs[0].split('-')[0], to:rs[0].split('-')[1]}
           console.log( page)
-       
+          console.log(response.body)
+          this.tech = response.body
+          this.page.loading = false
         }, response => {
-          this.tech = [];
-        });
+          return headersResponse
+        })
       },
       getDetails () {
         // 'content-range'
