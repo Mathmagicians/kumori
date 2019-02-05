b0084b85b309f1b4ee5893a87c366b6873437e87
Johannes Frandsen <jsf@greenoak.dk>
2019-02-05 21:30:23 +0100

    Enabled hot reload

--- services/frontend/config/index.js
+++ services/frontend/config/index.js
@@ -16,11 +16,12 @@ module.exports = {
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
-    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: 80, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
+    disableHostCheck: true,
 
     // Use Eslint Loader?
     // If true, your code will be linted during bundling and
