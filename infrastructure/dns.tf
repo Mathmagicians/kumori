ee39f8d2e7e1b8db9218d5d047704558c92e4d96
Johannes Frandsen <jsf@greenoak.dk>
2019-02-05 21:28:49 +0100

    Fixed starting order of services

--- infrastructure/dns.tf
+++ infrastructure/dns.tf
@@ -1,6 +1,14 @@
-resource "cloudflare_record" "test1" {
+resource "cloudflare_record" "demo" {
   domain = "${var.cloudflare_domain}"
   name   = "demo"
+  value  = "${digitalocean_droplet.dockerhost.ipv4_address}"
+  type   = "A"
+  ttl    = 1
+}
+
+resource "cloudflare_record" "app" {
+  domain = "${var.cloudflare_domain}"
+  name   = "app"
   value  = "${digitalocean_droplet.dockerhost.ipv4_address}"
   type   = "A"
   ttl    = 1
