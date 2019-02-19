3442065a04be2af81f3dd6c75cf87bff9e117b19
Agata Przybyszewska <agata@mathmagicians.dk>
2019-02-19 23:19:41 +0100

    Passing component as a prop. Icon chosen depending on status

--- services/frontend/src/components/Project.vue
+++ services/frontend/src/components/Project.vue
@@ -1,12 +1,51 @@
 <template>
-<div>
-  <h1>Content on the way ...</h1>
-</div>
+<b-card no-body :id="project.name">
+  <span slot="header" :v-b-toggle="accordionId">
+    <b-button variant="link">
+      {{project.name}}
+    </b-button>
+    {{project.status.value}}
+      <v-icon :name="statusIcon" color="grey" />
+  </span>
+  <div>
+    {{project.status}}
+  </div>
+
+</b-card>
 </template>
 
 <script>
+
+import Icon from 'vue-awesome/components/Icon'
+import 'vue-awesome/icons/bolt'
+import 'vue-awesome/icons/cloud'
+import 'vue-awesome/icons/sun'
+
 export default {
-  name: 'project'
+  name: 'project',
+  icons: Object.freeze(['meteor','bolt','cloud','cloud','sun']),
+  components: {
+    'v-icon': Icon
+  },
+  props: {
+    project: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    accordionId: function () {
+      return 'accordion-' + this.project.name;
+    },
+    statusIcon: function () {
+      return this.iconNameFromValue(this.project.status.value)
+    }
+  },
+  methods: {
+    iconNameFromValue: function(val) {
+        return val<2 ? 'sun' : 'bolt'
+    }
+  }
 }
 </script>
 
