3c4e7febfa36b9986c651193b1161c985a1d5085
Johannes Frandsen <jsf@greenoak.dk>
2019-02-20 08:50:55 +0100

    Moved project list function to api

--- services/frontend/src/views/Projects.vue
+++ services/frontend/src/views/Projects.vue
@@ -1,62 +1,16 @@
 <template>
 <div>
-
-    <b-alert show variant="secondary">
-      your projects
-    </b-alert>
-
-    <list-with-pagination ref="pagination" :list-size="projects.length" v-bind:list-total="projects.length" v-bind:activeOrdinal="0" :button-panel-size="7">
-      <project slot-scope="props"  v-if="props.isOn" :project="projects[props.listSeq]">
-      </project>
-    </list-with-pagination>
-
-  </div>
+  <h1>Projects</h1>
+  <list-with-pagination ref="pagination" :list-size="projects.length" v-bind:list-total="projects.length" v-bind:activeOrdinal="0" :button-panel-size="7">
+    <project slot-scope="props" v-if="props.isOn" :project="projects[props.listSeq]">
+    </project>
+  </list-with-pagination>
+</div>
 </template>
 
 <script>
-let data = [{
-  name: 'Project Suspicious',
-  status: {
-    value: 1,
-    trend: 1,
-    timestamp: '2018-08-21:00:00:00'
-  },
-  history: [{
-    value: 1,
-    trend: 1,
-    timestamp: '2018-08-01:00:00:00'
-  }, {
-    value: 1,
-    trend: 1,
-    timestamp: '2018-07-21:00:00:00'
-  }, {
-    value: 1,
-    trend: 1,
-    timestamp: '2018-06-01:00:00:00'
-  }]
-}, {
-  name: 'Project Awesome',
-  status: {
-    value: 5,
-    trend: -1,
-    timestamp: '2018-08-21:00:00:00'
-  },
-  history: [{
-    value: 1,
-    trend: 1,
-    timestamp: '2018-08-01:00:00:00'
-  }, {
-    value: 1,
-    trend: 1,
-    timestamp: '2018-07-21:00:00:00'
-  }, {
-    value: 1,
-    trend: 1,
-    timestamp: '2018-06-01:00:00:00'
-  }]
-}]
-
 import Project from '../components/Project.vue'
+import Projects from '../api/Projects.js'
 import ListWithPagination from '../components/ListWithPagination.vue'
 
 export default {
@@ -68,7 +22,7 @@ export default {
   data() {
     return {
       loading: false,
-      projects: data
+      projects: Projects.list()
     }
   }
 }
