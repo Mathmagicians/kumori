47c9a6bbe3ada6ea2301f5e8c3caaccc1b86bdfb
Johannes Frandsen <jsf@greenoak.dk>
2019-04-04 13:16:19 +0200

    Added link to survey

--- services/frontend/src/components/Project.vue
+++ services/frontend/src/components/Project.vue
@@ -4,6 +4,9 @@
     <b-button variant="link">
       {{project.name}}
     </b-button>
+    <b-button to="/survey" variant="link">
+      Project information
+    </b-button>
     {{project.status.value}}
       <weather :state="project.status.value" size="16" />
   </span>
