62a08fbffc96bfda6422a9236223c21e0a2ca014
Johannes Frandsen <jsf@greenoak.dk>
2019-04-11 08:25:28 +0200

    small fix to path

--- services/frontend/src/Auth/AuthService.js
+++ services/frontend/src/Auth/AuthService.js
@@ -1,7 +1,6 @@
 import auth0 from 'auth0-js'
 
 export default class AuthService {
-
   auth0 = new auth0.WebAuth({
     domain: 'mindfield.eu.auth0.com',
     clientID: 'QERGzVzudxY25M3TOa5rmcwVW8yWbzyg',
