ee3ff9bfef61b34e563bc3f3b9171bc5c50dfa97
Johannes Frandsen <jsf@greenoak.dk>
2019-04-16 22:21:19 +0200

    Simplified router

--- services/frontend/src/router/index.js
+++ services/frontend/src/router/index.js
@@ -1,88 +1,92 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import About from '../views/About.vue'
-import TechComponentsList from '../views/TechComponentList.vue'
-import Errors from '../views/Errors.vue'
-import Exterminator from '../views/Exterminator.vue'
-import Home from '../views/Home.vue'
-import Projects from '../views/Projects.vue'
-import Meritocracy from '../views/Meritocracy.vue'
-import Database from '../views/Database.vue'
-import Usecases from '../views/Usecases.vue'
-import Survey from '../views/Survey.vue'
-import TechComponentEditor from '../components/TechComponentEditor.vue'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   routes: [{
-      path: '/',
-      component: Home
-    }, {
-      path: '/about',
-      component: About
-    }, {
-      path: '/components',
-      component: TechComponentsList,
-      name: 'component-list'
-    }, {
-      path: '/exterminator',
-      component: Exterminator
-    }, {
-      path: '/projects',
-      component: Projects
-    }, {
-      path: '/survey',
-      component: Survey
-    }, {
-      path: '/survey',
-      component: Survey
-    },{
-      path: '/meritocracy',
-      component: Meritocracy
-    }, {
-      path: '/components/search',
-      component: TechComponentsList
-    }, {
-      path: '/components/:name',
-      component: TechComponentsList,
-      name: 'component',
-      props: true
-    }, {
-      path: '/components/:uid/edit',
-      name: 'edit',
-      component: TechComponentEditor,
-      props: true
-    }, {
-      path: 'components/create',
-      name: 'create',
-      component: TechComponentEditor,
-      props: true
-    }, {
-      path: '/db',
-      component: Database
-    }, {
-      path: '/usecases',
-      component: Usecases
-    }, {
-      path: '/404',
-      name: 'not-found',
-      component: Errors,
-      props: {
-        error: 404
-      }
-    }, {
-      path: '/error',
-      name: 'error',
-      component: Errors,
-      props: route => ({
-        code: route.query.error,
-        asset: route.query.asset
-      })
-    }, {
-      path: '*',
-      redirect: '/404'
+    path: '/',
+    component: () =>
+      import('../views/Home.vue')
+  }, {
+    path: '/about',
+    component: () =>
+      import('../views/About.vue')
+  }, {
+    path: '/list',
+    component: () =>
+      import('../views/List.vue')
+  }, {
+    path: '/components',
+    component: () =>
+      import('../views/TechComponentList.vue'),
+    name: 'component-list'
+  }, {
+    path: '/exterminator',
+    component: () =>
+      import('../views/Exterminator.vue')
+  }, {
+    path: '/projects',
+    component: () =>
+      import('../views/Projects.vue')
+  }, {
+    path: '/survey',
+    component: () =>
+      import('../views/Survey.vue')
+  }, {
+    path: '/meritocracy',
+    component: () =>
+      import('../views/Meritocracy.vue')
+  }, {
+    path: '/components/search',
+    component: () =>
+      import('../views/TechComponentList.vue')
+  }, {
+    path: '/components/:name',
+    component: () =>
+      import('../views/TechComponentList.vue'),
+    name: 'component',
+    props: true
+  }, {
+    path: '/components/:uid/edit',
+    name: 'edit',
+    component: () =>
+      import('../components/TechComponentEditor.vue'),
+    props: true
+  }, {
+    path: 'components/create',
+    name: 'create',
+    component: () =>
+      import('../components/TechComponentEditor.vue'),
+    props: true
+  }, {
+    path: '/db',
+    component: () =>
+      import('../views/Database.vue')
+  }, {
+    path: '/usecases',
+    component: () =>
+      import('../views/Usecases.vue')
+  }, {
+    path: '/404',
+    name: 'not-found',
+    component: () =>
+      import('../views/Errors.vue'),
+    props: {
+      error: 404
     }
-  ]
+  }, {
+    path: '/error',
+    name: 'error',
+    component: () =>
+      import('../views/Errors.vue'),
+    props: route => ({
+      code: route.query.error,
+      asset: route.query.asset
+    })
+  }, {
+    path: '*',
+    redirect: '/404'
+  }]
 })
